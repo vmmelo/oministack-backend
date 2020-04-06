786e88553a5cf133503ec00be0a0dff06f3b3b6a
Victor Miranda <vmmelo1@gmail.com>
2020-04-05 21:22:01 -0300

    error msg

--- src/controllers/SessionController.js
+++ src/controllers/SessionController.js
@@ -11,7 +11,7 @@ module.exports = {
             .first();
 
         if(!ong) {
-            return res.status(400).json({error: 'No ONG found with this ID.'})
+            return res.status(400).json({error: 'Nenhuma ONG foi encontrada com este ID.'})
         }
 
         return res.json(ong);
